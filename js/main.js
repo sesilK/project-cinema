e780f54e996e765a5d74ba1667b6301bf65adb75
sesilK <kimsesilxyz@gmail.com>
2023-04-04 16:16:58 +0900

    관람평 추가, 스틸컷 추가, 맨위로가기 버튼 추가, 자동슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -19,8 +19,6 @@ function like(obj) {
 }
 
 
-
-
 var colors = new Array(
     [21, 63, 46],
     [32, 23, 42],
@@ -42,7 +40,8 @@ var gradientSpeed = 0.003;
 
 function updateGradient() {
 
-    if ($ === undefined) return;
+    if ($ === undefined)
+        return;
 
     var c0_0 = colors[colorIndices[0]];
     var c0_1 = colors[colorIndices[1]];
@@ -81,3 +80,112 @@ function updateGradient() {
 }
 
 setInterval(updateGradient, 0.1);
+
+
+
+window.onload = function () {
+
+    const kindWrap = document.querySelector('.kind_wrap');
+    const slider = kindWrap.querySelector('.slider');
+    const slideLis = slider.querySelectorAll('li');
+    // const moveButton = kindWrap.querySelector('.arrow');
+
+    for (let i = 0; i < slideLis.length; i++) {
+        slideLis[i].style.width = `${document.querySelector('.kind_wrap').clientWidth}px`;
+    }
+
+    /* ul 넓이 계산 */
+    const liWidth = slideLis[0].clientWidth;
+    const sliderWidth = liWidth * slideLis.length;
+    slider.style.width = `${sliderWidth}px`;
+
+    /* 리스너 설치 */
+    let currentIdx = 0; // 슬라이드 현재 번호
+    let translate = 0; // 슬라이드 위치 값
+    // const speedTime = 500;
+    // moveButton.addEventListener('click', moveSlide);
+
+    /* 슬라이드 실행 */
+    function move() {
+        if (currentIdx !== slideLis.length - 1) {   //1~5번째 사진일때
+            translate -= liWidth;
+            slider.style.transition = `${0.5}s ease-out`;
+            slider.style.transform = `translateX(${translate}px)`;
+            currentIdx += 1;
+        } else if (currentIdx == slideLis.length - 1) {    //6번 사진일때
+            translate = 0;
+            slider.style.transition = `${0}s ease-out`;
+            slider.style.transform = `translateX(${translate}px)`;
+            currentIdx = 0;
+        }
+    }
+
+    function moveSlide(event) {
+        // event.preventDefault();
+
+        // if (event.target.className === 'next') {
+        //     if (currentIdx !== slideLis.length - 1) {   //1~4번째 사진일때
+        //         translate -= liWidth;
+        //         slider.style.transform = `translateX(${translate}px)`;
+        //         currentIdx += 1;
+        //     }
+        //     // else if (currentIdx == slideLis.length - 1){    //5번째 사진일때
+        //     //     translate -= liWidth;
+        //     //     slider.style.transform = `translateX(${translate}px)`;
+        //     //     currentIdx += 1;
+        //     //     slider.appendChild(list.appendChild(image));
+        //     // }
+        //     else if (currentIdx == slideLis.length - 1){
+        //         translate = 0;
+        //         slider.style.transform = `translateX(${translate}px)`;
+        //         currentIdx = 0;
+        //     }
+        // } else if (event.target.className === 'prev') {
+        //     if (currentIdx !== 0) {     //1번째 사진일때
+        //         translate += liWidth;
+        //         slider.style.transform = `translateX(${translate}px)`;
+        //         currentIdx -= 1;
+        //     }
+        //     // else if (currentIdx == 0 ) {        //2~5번째 사진일때
+        //     //     translate = -(liWidth * (slideLis.length-1));
+        //     //     slider.style.transform = `translateX(${translate}px)`;
+        //     //     currentIdx = slideLis.length - 1;
+        //     // }
+        //     else if (currentIdx == 0 ) {
+        //         translate = -(liWidth * (slideLis.length-1));
+        //         slider.style.transform = `translateX(${translate}px)`;
+        //         currentIdx = slideLis.length - 1;
+        //     }
+        // }
+    }
+
+    function sliding() {
+        // move();
+        // if (currentIdx === sliderCloneLis.length - 1)
+        //     setTimeout(() => {
+        //         translate -= liWidth;
+        //         slider.style.transform = `translateX(${translate}px)`;
+        //         currentIdx += 1;
+        //     }, speedTime);
+    }
+
+    let time = 750;
+    function showSliding() {
+        let add = 1500;
+            setTimeout(move, time); //2번째 사진으로 넘어갈때 750
+            time +=add;
+            setTimeout(move, time); //3번째 사진으로 넘어갈때 1500
+            time +=add;
+            setTimeout(move, time); //4번째 사진으로 넘어갈때 1500
+            time +=add;
+            setTimeout(move, time); //5번째 사진으로 넘어갈때 1500
+            time +=add;            
+            setTimeout(move, time); //6번째 사진으로 넘어갈때 1500
+            time +=750;
+            setTimeout(move, time); //1번째 사진으로 넘어갈때 750
+            time +=750;             //2번째 사진으로 넘어갈때 750
+    }
+
+    // setInterval(move, 1500);
+    setInterval(showSliding, 0);
+}
--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,118 @@
-// function seeMore() {
-//     document.querySelector(".unhidden").classList.toggle("hidden");
-// }
 
+
+const inputBar = document.querySelector("#comment-input");
+const rootDiv = document.querySelector("#comments");
+const btn = document.querySelector("#submit");
+const mainCommentCount = document.querySelector('#count'); //맨위 댓글 숫자 세는거.
+
+
+
+//타임스템프 만들기
+function generateTime() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const wDate = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    const time = year + '-' + month + '-' + wDate + ' ' + hour + ':' + min + ':' + sec;
+    return time;
+
+}
+
+//유저이름 발생기
+//유저이름은 8글자로 제한.
+function generateUserName() {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var makeUsername = '';
+    for (let i = 0; i < 4; i++) {
+        let index = Math.floor(Math.random(10) * alphabet.length);
+        makeUsername += alphabet[index];
+    }
+    for (let j = 0; j < 4; j++) {
+        makeUsername += "*";
+    }
+    return makeUsername;
+}
+
+
+function deleteComments(obj) {
+    obj.parentNode.parentNode.remove();
+    //메인댓글 카운트 줄이기.
+    if (mainCommentCount.innerHTML <= '0') {
+        mainCommentCount.innerHTML = 0;
+    } else {
+        mainCommentCount.innerHTML--;
+    }
+}
+
+
+//댓글보여주기
+function showComment(comment) {
+    const userName = document.createElement('div');
+    const inputValue = document.createElement('span');
+    const showTime = document.createElement('div');
+    // const voteDiv = document.createElement('div');
+    const countSpan = document.createElement('span')
+    const commentList = document.createElement('div');
+
+    //삭제버튼 만들기
+    const delBtn = document.createElement('button');
+    delBtn.className = "deleteComment";
+    delBtn.innerHTML = "삭제";
+
+    commentList.className = "eachComment";
+    userName.className = "name";
+    inputValue.className = "inputValue";
+    showTime.className = "time";
+    // voteDiv.className = "voteDiv";
+
+    //유저네임가져오기 
+    userName.innerHTML = generateUserName();
+    userName.appendChild(delBtn);
+    //입력값 넘기기
+    inputValue.innerText = comment;
+    //타임스템프찍기
+    showTime.innerHTML = generateTime();
+    countSpan.innerHTML = 0;
+
+    //댓글뿌려주기       
+    commentList.appendChild(userName);
+    commentList.appendChild(inputValue);
+    commentList.appendChild(showTime);
+    // commentList.appendChild(voteDiv);
+    document.querySelector("#comments").prepend(commentList);
+    // console.dir(rootDiv);
+}
+
+
+//버튼만들기+입력값 전달
+function pressBtn() {
+    const currentVal = document.querySelector("#comment-input").value;
+    if (currentVal.length == 0) {
+        swal('내용을 입력해주세요');
+
+    } else {
+        showComment(currentVal);
+        document.querySelector('#count').innerHTML++;
+        document.querySelector("#comment-input").value = '';
+    }
+}
+
+function img_count() {
+    const img_div = document.querySelector(".section2").childNodes[1];
+    let img_count = 0;
+
+    for (let i = 0; i < img_div.children.length; i++) {
+        img_count += img_div.children[i].children.length;
+    }
+    console.log(img_count);
+    document.querySelector(".SteelCut").childNodes[1].innerText = img_count;
+}
+
+window.onload = function() {
+
+    img_count();
+}
